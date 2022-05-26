ab5b3e68fd084bdc4f07d2786deaa2628e7fdc41
unknown <xworkzodc@gmail.com>
2022-05-26 11:06:46 +0530

    Java Programs

---
+++ JAVA BTM/Control flow statement/DoWhileLoop.java
@@ -0,0 +1,17 @@
+class DoWhileLoop{
+
+ public static void main(String[] args) {
+
+ int x=10; 
+
+ do{
+
+  System.out.println("Saturday and Sunday Holiday");
+
+  }
+
+ while(x<=10); 
+
+
+     }
+  }
---
+++ JAVA BTM/Control flow statement/NestedForLoopDemo.java
@@ -0,0 +1,16 @@
+class NestedForLoopDemo {
+
+ public static void main(String[] args) {
+
+ int n=3; int k=2;
+
+ for(int i=0; i<n; i++) {
+
+ for(int j=0; j<k; j++) {
+
+ System.out.println("Hello Xworkz team" + j);
+
+      }
+    }
+  }
+}
