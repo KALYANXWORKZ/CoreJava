85e9d09f19ff1727958e59df86bf36f363c898ac
unknown <xworkzodc@gmail.com>
2022-06-08 23:20:58 +0530

    method

---
+++ JAVA BTM/Control flow statement/ContinueDemo.java
@@ -0,0 +1,14 @@
+class MethodDemo
+{
+static int square (int num)
+{
+return num*num;
+}
+public static void main(String[] args)
+{
+
+System.out.println(square(2));
+
+System.out.println(square(5));
+}
+}
