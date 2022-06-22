8f1488ce134f320d86aae7d6540a4d646efd7808
com <xworkzodc@gmail.com>
2022-06-23 00:04:41 +0530

    hashCode

---
+++ CoreJava/src/com/xworkz/core_java/ObjectClass/hashCode/HashCodeTest.java
@@ -0,0 +1,12 @@
+package com.xworkz.core_java.ObjectClass.hashCode;
+
+public class HashCodeTest {
+
+	public static void main(String[] args) {
+
+		Pen pen = new Pen();
+		System.out.println("Pens hascode:"+pen.hashCode());
+
+	}
+
+}
