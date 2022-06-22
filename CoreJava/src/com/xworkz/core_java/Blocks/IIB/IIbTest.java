6b4704008e06d11b84def44d6aa3ed9f1bddf7bb
com <xworkzodc@gmail.com>
2022-06-22 23:45:01 +0530

    Blocks

---
+++ CoreJava/src/com/xworkz/core_java/Blocks/IIB/IIbTest.java
@@ -0,0 +1,13 @@
+package com.xworkz.core_java.Blocks.IIB;
+
+public class IIbTest {
+
+	public static void main(String[] args) {
+		Television tv = new Television();
+		System.out.println("Brand Name is :" + tv.brand);
+		System.out.println("Type of the Tv is:"+ tv.type);
+		System.out.println("Price of Tv is:"+tv.price);
+		System.out.println("The Dimension:"+tv.dimension);
+	}
+
+}
---
+++ CoreJava/src/com/xworkz/core_java/Blocks/SIB/Staticibs.java
@@ -0,0 +1,29 @@
+package com.xworkz.core_java.Blocks.SIB;
+
+public class Staticibs 
+{
+	String brand;
+	String type;
+	double price;
+	String dimension;
+	static String dthProvider;
+
+	static 
+	{ 
+		System.out.println("I am SIB");
+		dthProvider = "cabel";
+	}
+
+	Staticibs()
+	{
+		System.out.println("Hello this is constructor calling from Blocks (SBI)!!!");
+	}
+
+	{
+		this.brand ="LG";
+		this.type = "LED";
+		this.price = 2200000.55;
+		this.dimension = "24.5inch";
+		System.out.println("Hello this is IbiS");
+	}
+}
