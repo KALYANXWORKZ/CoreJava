b33d8bb4d5f997e64da447a1dba840c6a704fb9f
com <xworkzodc@gmail.com>
2022-06-22 14:38:59 +0530

    Hierarchical_Inheritance

---
+++ CoreJava/src/com/xworkz/core_java/Inheritance/Hierarchical_Inheritance/Movie.java
@@ -0,0 +1,26 @@
+package com.xworkz.core_java.Inheritance.Hierarchical_Inheritance;
+
+class Movie
+{
+	void movietime()
+	{
+		System.out.println("We are going to watch movie");
+	}
+}
+
+class Kgf2 extends Movie
+{
+	void kgf2hit()
+	{
+		System.out.println("Kgf2 is hit blockbuster in worldwide");
+	}
+}
+
+class Vikrantrona extends Movie
+{
+	void vikrantronarelease()
+	{
+		System.out.println("Vikrantrona will be release worldwide");
+	}
+}
+
---
+++ CoreJava/src/com/xworkz/core_java/Inheritance/Hierarchical_Inheritance/Movieinheritance.java
@@ -0,0 +1,16 @@
+package com.xworkz.core_java.Inheritance.Hierarchical_Inheritance;
+
+class Movieinheritance
+{
+	public static void main(String []args)
+	{
+
+		Movie m = new Movie();
+		Kgf2 k = new Kgf2();
+		Vikrantrona v = new Vikrantrona();
+		v.movietime();
+		v.vikrantronarelease();
+		k.movietime();
+		k.kgf2hit();
+	}
+}
