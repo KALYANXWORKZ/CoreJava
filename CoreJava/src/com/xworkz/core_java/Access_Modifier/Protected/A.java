e1e68baa444127884f7bb12d72c93c4ff14f997a
com <xworkzodc@gmail.com>
2022-06-23 08:28:04 +0530

    Access_Modifier.Protected

---
+++ CoreJava/src/com/xworkz/core_java/Access_Modifier/Protected/A.java
@@ -0,0 +1,10 @@
+package com.xworkz.core_java.Access_Modifier.Protected;
+
+public class A
+{  
+		public void msg()
+		{
+			System.out.println("Hello");
+		}
+		 
+}
---
+++ CoreJava/src/com/xworkz/core_java/Access_Modifier/Protected/B.java
@@ -0,0 +1,10 @@
+package com.xworkz.core_java.Access_Modifier.Protected;
+
+class B
+{  
+	public static void main(String args[])
+	{  
+		A obj = new A();  
+		obj.msg();  
+	}  
+}
