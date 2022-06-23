70b6b6d62e85736ff3152679f108588e283e9f08
com <xworkzodc@gmail.com>
2022-06-23 08:47:37 +0530

    This_And_Super

---
+++ CoreJava/src/com/xworkz/core_java/This_And_Super/Food.java
@@ -0,0 +1,9 @@
+package com.xworkz.core_java.This_And_Super;
+
+public class Food
+{
+	String name = "pfulka";
+	String taste = "Yummy";
+	String color = "Light_";
+	double price = 25.00;
+}
---
+++ CoreJava/src/com/xworkz/core_java/This_And_Super/FoodTest.java
@@ -0,0 +1,9 @@
+package com.xworkz.core_java.This_And_Super;
+
+public class FoodTest
+{
+	public static void main(String[] args) 
+	{
+		Idly idly = new Idly("Vada","Cripsy","Brown",25.55);
+	}
+}
