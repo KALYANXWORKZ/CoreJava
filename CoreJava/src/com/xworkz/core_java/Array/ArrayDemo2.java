e519d1afcd145b2941fec710a77126b14b78efdb
com <xworkzodc@gmail.com>
2022-07-04 10:50:47 +0530

    Array

---
+++ CoreJava/src/com/xworkz/core_java/Array/ArrayDemo2.java
@@ -0,0 +1,19 @@
+package com.xworkz.core_java.Array;
+
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		char [] alphabet = new char[26];
+		char arr = 'A';
+		for(char i=0; i<alphabet.length;i++) {
+		alphabet[i] = arr;
+		arr++;
+		System.out.println(alphabet[i]+" ");		
+			
+			
+		}
+		
+
+	}
+
+}
