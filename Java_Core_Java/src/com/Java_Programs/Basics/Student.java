f2877f50473ad7e9338c652e9131d2c6a67cc8d9
com <xworkzodc@gmail.com>
2022-07-21 18:22:00 +0530

    Basic Programs

---
+++ Java_Core_Java/src/com/Java_Programs/Basics/Student.java
@@ -0,0 +1,16 @@
+package com.Java_Programs.Basics;
+
+public class Student {
+	
+	public static void main(String[] args) {
+		
+		int age;       // variable declaration
+		age = 20;      // variable initialization
+		
+		String name = "Jerry";    // variable declaration and initialization
+		
+		System.out.println("Student age is: "+age);
+		System.out.println("Student name is: "+name);
+	}
+
+}
