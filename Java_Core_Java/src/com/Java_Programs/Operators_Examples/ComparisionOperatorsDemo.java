d18334d6c545f4756e087f3b39946c98afe8ccb1
com <xworkzodc@gmail.com>
2022-07-22 12:06:38 +0530

    Operators

---
+++ Java_Core_Java/src/com/Java_Programs/Operators_Examples/ComparisionOperatorsDemo.java
@@ -0,0 +1,18 @@
+package com.Java_Programs.Operators_Examples;
+
+public class ComparisionOperatorsDemo {
+	public static void main(String[] args) {
+		int x = 20;
+		int y = 10;
+		System.out.println(x<y);
+		System.out.println(x<=y);
+		System.out.println(x>y);
+		System.out.println(x>=y);
+		System.out.println(x==20);
+		System.out.println(x!=y);
+		System.out.println(x!=10);
+
+
+	}
+
+}
---
+++ Java_Core_Java/src/com/Java_Programs/Operators_Examples/LogicalOperatorsDemo.java
@@ -0,0 +1,11 @@
+package com.Java_Programs.Operators_Examples;
+
+public class LogicalOperatorsDemo {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		boolean result = a<b && a==1;
+		System.out.println(result);
+		
+	}
+}
