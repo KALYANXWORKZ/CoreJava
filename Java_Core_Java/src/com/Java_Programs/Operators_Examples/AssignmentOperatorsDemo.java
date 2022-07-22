40cc21e5269cfbd6e3042339116d3ff0f733dad8
com <xworkzodc@gmail.com>
2022-07-22 12:05:50 +0530

    Hello

---
+++ Java_Core_Java/src/com/Java_Programs/Operators_Examples/AssignmentOperatorsDemo.java
@@ -0,0 +1,16 @@
+package com.Java_Programs.Operators_Examples;
+
+public class AssignmentOperatorsDemo {
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println("Value of x = "+x);
+		x+=20;
+		System.out.println("Value of x = "+x);
+		
+        int i = 45;
+        System.out.println("Value of i = "+i);
+        i-=13;
+        System.out.println("Value of i = "+i);
+   
+	}
+}
