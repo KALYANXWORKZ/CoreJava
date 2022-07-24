4934afaa035750ab2be3441c33249facc1575ffd
com <xworkzodc@gmail.com>
2022-07-24 21:56:11 +0530

    HashSet

---
+++ CoreJava/src/com/xworkz/core_java/Collection_Framework/Sets_/HashSet/HashSetDemo.java
@@ -0,0 +1,19 @@
+package com.xworkz.core_java.Collection_Framework.Sets_.HashSet;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+	public static void main(String[] args) {
+		HashSet hs=new HashSet();
+		hs.add(2);
+		hs.add(1);
+		hs.add(1);
+		hs.add("null");
+		hs.add("null");
+		hs.add("Java");
+
+		System.out.println(hs);
+
+	}
+
+}
