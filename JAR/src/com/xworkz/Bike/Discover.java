a3bae2474a6341621460622cc582d1acb691ec46
com <xworkzodc@gmail.com>
2022-07-27 14:34:58 +0530

    JAR

---
+++ JAR/src/com/xworkz/Bike/Discover.java
@@ -0,0 +1,12 @@
+package com.xworkz.Bike;
+
+public class Discover {
+
+	public Discover() {
+		System.out.println("Bike Brand is Discover");
+	}
+	String Kalyan(){
+		return "KalyanKumar";
+
+	}
+}
---
+++ JAR/src/com/xworkz/Bike/Test.java
@@ -0,0 +1,15 @@
+package com.xworkz.Bike;
+
+public class Test {
+	public static void main(String[] args) {
+		
+   System.out.println("Main method is Started");
+   
+		Discover d = new Discover();
+		
+		String k= d.Kalyan();
+		
+		System.out.println(k);
+		
+	}
+}
