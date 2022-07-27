463baf49da4c07d371b653d0bbd30a4c2eacba6b
com <xworkzodc@gmail.com>
2022-07-27 18:20:49 +0530

    Jar Example 2

---
+++ JarExamples/src/com/xworkz/vehicle/Bike.java
@@ -0,0 +1,13 @@
+package com.xworkz.vehicle;
+
+public class Bike {
+	public static void main(String[] args) {
+		System.out.println("Main method is statred");
+
+		VehicleExample ve = new VehicleExample();
+
+		int v = ve.getPrice();
+
+		System.out.println("Price of the bike is : " +v);
+	}
+}
---
+++ JarExamples/src/com/xworkz/vehicle/VehicleExample.java
@@ -0,0 +1,13 @@
+package com.xworkz.vehicle;
+
+public class VehicleExample {
+	
+	public VehicleExample() {
+		System.out.println("Vehicle name is Bike");
+	}
+	
+	int getPrice(){
+		
+		return 86500;
+	}
+}
