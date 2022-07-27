270632402b5e7cfe7b6d9d5c7e9c947b05971f3d
com <xworkzodc@gmail.com>
2022-07-27 13:51:22 +0530

    JAR

---
+++ JAR/src/com/xworkz/kissanJam/Jam.java
@@ -0,0 +1,12 @@
+package com.xworkz.kissanJam;
+
+public class Jam {
+	public static void main(String[] args) {
+		Vendor v = new Vendor();
+
+		String a = v.getName();
+
+		System.out.println(a);
+
+	}
+}
---
+++ JAR/src/com/xworkz/kissanJam/Vendor.java
@@ -0,0 +1,12 @@
+package com.xworkz.kissanJam;
+
+public class Vendor {
+
+	public Vendor() {
+		System.out.println("Vendor name is Chandru");
+	}
+
+	public String getName() {
+		return "Google";
+	}
+}
